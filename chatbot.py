9d5f0a706bd0278fc314acc5c87547ba4e0baca9
Luis Galocha <galdomluis@gmail.com>
2022-01-10 17:07:37 +0100

    cambia algunas cosas del formato de los textos para que sea mas sencillo de tratar

--- chatbot.py
+++ chatbot.py
@@ -88,11 +88,13 @@ if False:
 data = open(fichero,'r')
 texto = data.read() 
 
-r = re.compile(r'(\b[\w.]+@+[\w.]+.+[\w.]\b)')
+r = re.compile(r'(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?')
 results = r.findall(texto)
 emails = ""
 for x in results:
         emails += str(x)+"\n"
+
+print(emails)
  # Esto nos da todos los emails que contiene el texto
 '''
 new_columns = ['Número']
