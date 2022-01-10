7c8e0413a08f053823357fb6624d07f4171ee1ac
Luis Galocha <galdomluis@gmail.com>
2022-01-10 10:33:02 +0100

    empieza con el chatbot y añade el directorio de los documentos

---
+++ chatbot.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import spacy
+import es_core_news_sm
+from spacy.matcher import Matcher
+
+rutas = ["cv_txt/cv_jorge.txt", "cv_txt/cv_luis.txt"]
+
+indice = int(input("Escribe 0 para elegir el curriculum de jorge o 1 para elegir el curriculum de luis: "))
+
+while (indice not in range(2)):
+    indice = int(input("Escribe 0 para elegir el curriculum de jorge o 1 para elegir el curriculum de luis: "))
+
+fichero = rutas[indice]
+
+# ,pregunta = input("Escriba su pregunta: ")
+
+with open(fichero, 'r') as f:
+    text = [line for line in f.readlines()]
+
+df = pd.DataFrame(text,columns=['text'])
+df.head()
+
+text = df['text'][0]
+nlp = es_core_news_sm.load()
+doc = nlp(text)
+
+features = []
+for token in doc:
+    features.append({'token' : token.text, 'pos' : token.pos_})
+
+fdf = pd.DataFrame(features)
+fdf.head(len(fdf))
+
+first_tokens = ['to', 'father']
+last_tokens = ['and', 'naming']
+
+pattern_father = [[
+           {'POS':'PROPN', 'OP' : '+'},
+           ]]
+
+def encontrar_nombre(x):
+    nlp = es_core_news_sm.load()
+    doc = nlp(x)
+    matcher = Matcher(nlp.vocab) 
+    matcher.add("matching_father", pattern_father)
+    matches = matcher(doc)
+    sub_text = ''    
+    if(len(matches) > 0):
+        span = doc[matches[0][1]:matches[0][2]] 
+        sub_text = span.text
+    tokens = sub_text.split(' ')
+    
+    
+    name, surname = tokens[1:-1]
+    return name, surname
+
+for n,col in enumerate(new_columns):
+    df[col] = df['text'].apply(lambda x: encontrar_nombre(x)).apply(lambda x: x[n])
+
+print(df)
