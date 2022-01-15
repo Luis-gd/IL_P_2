acd85bb5e5588c4679cd320d83b93dae822bccdb
Atarata <jorgeangulo_va@outlook.com>
2022-01-15 23:15:20 +0100

    Selector de preguntas

--- chatbot.py
+++ chatbot.py
@@ -2,8 +2,52 @@ import pandas as pd
 import spacy
 import es_core_news_sm
 from spacy.matcher import Matcher
+from scipy import spatial
 import re
+import math
 import unicodedata
+import unidecode
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from operator import itemgetter
+
+
+PREGUNTAS = {
+    #IDIOMAS 4
+    '¿Qué idiomas habla?':4, '¿Qué idiomas conoce el candidato?': 4,
+    '¿Cuáles son los idiomas que conoce el candidato?': 4, '¿Qué lenguas habla el candidato?': 4,
+    '¿Qué lenguas son las que conoce el candidato?': 4, '¿Idiomas?': 4,
+    #Nombre del candidato 0
+    '¿Cómo se llama el candidato?': 0, '¿Cuál es el nombre del candidato?:': 0,
+    '¿Cómo se llama el candidato?': 0, '¿Con qué nombre responde el candidato?': 0,
+    '¿Quién es el candidato?': 0, '¿Nombre?': 0,
+    #Teléfono del candidato 2
+    '¿Cuál es el número de teléfono del candidato?': 2, '¿Qué número de teléfono tiene el candidato?': 2,
+    '¿A qué número hay que llamar para contactar con el candidato?': 2, '¿Qué número de teléfono figura como el número del candidato?': 2,
+    '¿Teléfono de contacto?': 2,
+
+    #Experiencia laboral 7
+    '¿Qué experiencia laboral tiene?': 7, '¿Qué experiencia tiene el candidato?': 7,
+    '¿Cuál es la experiencia que tiene el candidato?': 7, '¿Qué tipo de experiencia tiene el candidato?':7,
+    '¿Dónde ha trabajado previamente el candidato?': 7, '¿Qué profesiones ha llevado a cabo previamente el candidato?': 7,
+    '¿Experiencia?': 7,
+    #Lengiajes de programación 3
+    '¿Qué lenguajes de programación conoce?': 3, '¿Qué lenguajes de programación controla el candidato?':3,
+    '¿Cuáles son los lenguajes de programación que controla el candidato?': 3, '¿Cómo se llaman los lenguajes de programación que controla el candidato?': 3,
+    '¿De qué lenguajes de programación tiene conocimientos el candidato?':3, '¿Lenguajes de programación?': 3,
+    #Formación académica 6
+    '¿Qué educación tiene el candidato?': 6, '¿Qué títulos tiene el candidato?': 6,
+    '¿Cuáles son los títulos del candidato?':6, '¿Con qué titulaciones cuenta el candidato?': 6,
+    '¿Qué ha estudiado?': 6, '¿Qué estudios tiene?': 6,
+    #Correo de contacto 1
+    '¿Cuál es su correo de contacto?':1, '¿Cuál es el correo del candidato?': 1,
+    '¿Qué correo tiene el candidato?': 1, '¿A qué dirección de correo debemos escribir para contactar con el candidato?': 1,
+    '¿Qué dirección de correo figura como la dirección del candidato?': 1, '¿Correo?': 1,
+    #Referencias 5
+    '¿Cuáles son sus referencias?': 5, '¿Cuáles son las referencias del candidato?': 5,
+    '¿Qué referencias tiene el candidato?': 5, '¿Qué personas pueden servir como referencia del candidato?': 5,
+    '¿Quién figura como referencia del candidato?': 5, '¿Referencias?' : 5
+}
 
 secciones = ['EXPERIENCIA', 'EDUCACIÓN', 'CONTACTO', 'HABILIDADES', 'IDIOMAS', 'REFERENCIAS']
 
@@ -20,7 +64,7 @@ def encontrar_seccion(seccion):
         if fila_i != None and all(v) and f!= fila_i and len(v) != 0:
             fila_f = f
             break
-    return (fila_i, fila_f) 
+    return (fila_i, fila_f)
 
 def saca_texto(lineas):
     i, f = lineas
@@ -36,12 +80,12 @@ def saca_texto(lineas):
 def encontrar_nombre(x):
     nlp = es_core_news_sm.load()
     doc = nlp(x)
-    matcher = Matcher(nlp.vocab) 
+    matcher = Matcher(nlp.vocab)
     matcher.add("matching_father", pattern_father)
     matches = matcher(doc)
-    sub_text = ''    
+    sub_text = ''
     if(len(matches) > 0):
-        span = doc[matches[0][1]:matches[0][2]] 
+        span = doc[matches[0][1]:matches[0][2]]
         sub_text = span.text
     tokens = sub_text.split(' ')
 
@@ -56,7 +100,7 @@ def devolver_nombre(x):
     for f in range(len(df['text'])):
         if df['Nombre'][f] != None and df['Apellido1'][f] != None and df['Apellido2'][f] != None:
             if f == 0:
-                nombre = df['Nombre'][f] + ' ' + df['Apellido1'][f] + ' ' + df['Apellido2'][f] 
+                nombre = df['Nombre'][f] + ' ' + df['Apellido1'][f] + ' ' + df['Apellido2'][f]
     return nombre
 
 last_token = ['\n']
@@ -82,6 +126,135 @@ def encontrar_lenguajes():
     l = [i for i in cadena if i in lenguajes.replace(',','').split()]
     return ', '.join(l)
 
+def tf_idf(frase, idf, union, k):
+
+    tokenizer = RegexpTokenizer(r'[A-Za-z_À-ÿ]{3,}')
+    palabras_vacias = set(stopwords.words('spanish'))
+
+    tf = dict.fromkeys(union,0)
+    tf_idf = dict.fromkeys(union,0)
+    #TF
+    frase_tokens = tokenizer.tokenize(frase)
+    frase_tokens = [w for w in frase_tokens if not w.lower() in palabras_vacias]
+
+    for palabra in frase_tokens:
+        tf[palabra] += 1
+
+    for palabra in frase_tokens:
+        tf_idf[palabra] = idf[palabra] * tf[palabra]
+
+    l = sorted(tf_idf.items(), key=itemgetter(1), reverse=True)
+    l = l[:k]
+    #Compute array
+    arr = []
+    for palabra in union:
+        match = False
+        for par in l:
+            if palabra == par[0]:
+                match = True
+                break
+        if match:
+            arr.append(1)
+        else:
+            arr.append(0)
+    return arr
+
+def limpiar_pregunta(cadena):
+    cadena_procesada = unidecode.unidecode(cadena)  #Quitar acentos
+    cadena_procesada = cadena_procesada.lower()  # A minúsculas
+    cadena_procesada = re.sub(r"[^a-z0-9 ]", "",cadena_procesada) #Quitar  carácteres especiales
+
+    return cadena_procesada
+
+#Tomar una pregunta y buscar la más similar
+def preocesar_pregunta(pregunta):
+
+    tokenizer = RegexpTokenizer(r'[A-Za-z_À-ÿ]{3,}')
+    palabras_vacias = set(stopwords.words('spanish'))
+
+    #Preprocesar pregunta:
+    pregunta_procesada = limpiar_pregunta(pregunta)
+    texto = pregunta_procesada + ' '
+
+    #Corpus de preguntas
+    for q in PREGUNTAS:
+        #Si está en la lista, terminamos aquí
+        if ( pregunta_procesada in limpiar_pregunta(q) ) or ( limpiar_pregunta(q) in pregunta_procesada ):
+            return [q, PREGUNTAS[q], 1]
+
+        q_procesada = limpiar_pregunta(q) + ' '
+        texto += q_procesada
+
+
+    texto = tokenizer.tokenize(texto)
+    texto = [w for w in texto if not w.lower() in palabras_vacias]
+
+    #IDF
+    union = []
+    N = len(texto)
+    for palabra in texto:
+        union = set(union).union([palabra])
+
+    #print('Union  '+str(union))
+
+    IDF = dict.fromkeys(union,0)
+    for palabra in texto:
+        IDF[palabra] += 1
+
+    for palabra in texto:
+        IDF[palabra] = math.log10((N/(1+IDF[palabra])))
+    #Buscamos la frase más cercana
+    v1 = tf_idf(pregunta_procesada, IDF, union, 15)
+    max = 0
+    cercana = []
+    for q in PREGUNTAS:
+        q_procesada = limpiar_pregunta(q)
+        v2 = tf_idf(q_procesada, IDF, union, 15)
+        d = 1 - spatial.distance.cosine(v1, v2)
+        if d > max:
+            max = d
+            cercana = [q, PREGUNTAS[q], d]
+
+    return cercana
+
+def preguntar_pregunta(pregunta):
+        #Cotas de similitud:
+        cota1 = 0.8
+        cota2 = 0.4
+        vector = preocesar_pregunta(pregunta)
+        if vector[2] >= cota1:
+            selector = vector[1]
+        elif vector[2] >= cota2:
+            selector = vector[1]
+            print("\n\n¿Es esta su pregunta: (s/n)?\n\n")
+            print("\n\n"+vector[0]+"\n\n")
+            confirm = input()
+            if not confirm.lower() == 's' or not confirm.lower() == 'si' or not confirm.lower() == 'sí':
+                selector = -1
+        else:
+            selector = -1
+
+        return selector
+def similitud_coseno(frase1, frase2):
+    idice_a_palabra = set(model.wv.index2word)
+    v1 = vector_medio(frase1, modelo = model, n=300, index2word_set=idice_a_palabra)
+    v2 = vector_medio(frase2, modelo = model, n=300, index2word_set=idice_a_palabra)
+    d = 1 - spatial.distance.cosine(v1, v2)
+
+    return d
+
+def vector_medio(frase, modelo, n, index2word_set):
+
+    palabras = frase.split()
+    vector_c = np.zeros((n, ), dtype='float32')
+    n_palabras= 0
+    for palabra in palabras:
+        if palabra in index2word_set:
+            n_palabras += 1
+            vector_c = np.add(vector_c, model[palabra])
+    if (n_palabras > 0):
+        vector_c = np.divide(vector_c, n_palabras)
+    return vector_c
 
 def main():
     parada = False
@@ -91,15 +264,15 @@ def main():
 
         indice = input(("Escriba 0 para elegir el primer currículum, "
         "1 para elegir el segundo currículum o "
-        "cualquier otra cosa para salir del sistema de preguntas:"))
+        "cualquier otra cosa para salir del sistema de preguntas:\n"))
 
         if indice.isdigit():
-            if indice not in range(2):
-                print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
+            if int(indice) not in range(2):
+                print("GRACIAS por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
                 parada = True
                 break
             else:
-                indice = int(indice) 
+                indice = int(indice)
         else:
             print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
             parada = True
@@ -108,53 +281,26 @@ def main():
         fichero = rutas[indice]
 
         #Cambiamos la variable pregunta para elegir qué dato tomamos
-        # Las preguntas está hechas de esta forma hasta que sepamos qué preguntas base y qué parafraseo poner. 
-        # Los números llegan hasta el 7, que son las dimensiones que tenemos actualmente. 
+        # Las preguntas está hechas de esta forma hasta que sepamos qué preguntas base y qué parafraseo poner.
+        # Los números llegan hasta el 7, que son las dimensiones que tenemos actualmente.
         # Si queremos añadir más solo habría que aumentar el range
 
-        pregunta = input(("¿Qué quieres saber sobre el candidato? \n"
-            "Para saber su nombre completo introduce 0. \n"
-            "Para saber su correo electrónico pulsa 1. \n"
-            "Para saber los idiomas que maneja pulsa 3. \n"
+        pregunta = input(("\n\n¿Qué quieres saber sobre el candidato? \n\n"
         ))
 
-        if pregunta.isdigit():
-            if pregunta not in range(2):
-                print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
-                parada = True
-                break
-            else:
-                pregunta = int(pregunta) 
-        else:
-            print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
-            parada = True
-            break
+        selector = preguntar_pregunta(pregunta)
+        print(selector)
 
-        while pregunta not in range(8):
-            
-            pregunta = int(input(("¿Qué quieres saber sobre el candidato? \n"
-                "Para saber su nombre completo introduce 0. \n"
-                "Para saber su correo electrónico pulsa 1. \n"
-                "Para saber los idiomas que maneja pulsa 3. \n"
-            )))
-            if pregunta.isdigit():
-                if pregunta not in range(2):
-                    print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
-                    parada = True
-                    break
-                else:
-                    pregunta = int(pregunta) 
-            else:
-                print("Gracias por hacer uso de este servicio. Le esperamos cuando tenga alguna duda.")
-                parada = True
-                break
+        while selector not in range(8):
+            selector = preguntar_pregunta(pregunta)
+            print(selector)
 
-            
+
         # Procesamiento del txt
 
         with open(fichero, 'r') as f:
             text = [line for line in f.readlines()]
-            
+
         df = pd.DataFrame(text,columns=['text'])
         df.head()
 
@@ -170,16 +316,16 @@ def main():
         fdf.head(len(fdf))
 
         # Llamada para sacar el nombre del candidato
-        if pregunta == 0:
+        if selector == 0:
             new_columns = ['Nombre','Apellido1', 'Apellido2']
             for n,col in enumerate(new_columns):
                 df[col] = df['text'].apply(lambda x: encontrar_nombre(x)).apply(lambda x: x[n])
             print("El nombre completo del candidato es:", devolver_nombre(df))
 
         # Llamada para sacar el correo del candidato
-        if pregunta == 1:
+        if selector == 1:
             data = open(fichero,'r')
-            texto = data.read() 
+            texto = data.read()
 
             r = re.compile(r'(\b[\w.]+@+[\w.]+.+[\w.]\b)')
             results = r.findall(texto)
@@ -188,9 +334,9 @@ def main():
 
         # Llamada para sacar el número de teléfono del candidato
         # Te dejo la estructura del email porque podría sacarse de forma similar
-        if pregunta == 2:
+        if selector == 2:
             data = open(fichero,'r')
-            texto = data.read() 
+            texto = data.read()
 
             r = re.compile(r'(\b[\w.]+@+[\w.]+.+[\w.]\b)')
             results = r.findall(texto)
@@ -198,23 +344,23 @@ def main():
             print("El teléfono del candidato es: ESTÁ POR HACER")
 
         # Llamada a sacar los lenguajes de programación que conoce el candidato
-        if pregunta == 3:
-            print('Los lenguajes de programación que maneja el candidato son:', encontrar_lenguajes() + '.') 
+        if selector == 3:
+            print('Los lenguajes de programación que maneja el candidato son:', encontrar_lenguajes() + '.')
 
         # Llamada para sacar los idiomas que sabe el candidato
-        if pregunta == 4:
+        if selector == 4:
             print("Los idiomas que maneja el candidato son:", encontrar_seccion(secciones[4]))
 
         # Llamada para sacar las referencias del candidato
-        if pregunta == 5:
+        if selector == 5:
             print("Las referencias del candidato son:", encontrar_seccion(secciones[5]))
 
         # Llamada para sacar la información académica del candidato
-        if pregunta == 6:
+        if selector == 6:
             print("La formación académica del candidato es:", encontrar_seccion(secciones[1]))
 
         # Llamada para sacar la información académica del candidato
-        if pregunta == 7:
+        if selector == 7:
             print("La experiencia laboral del candidato es:", encontrar_seccion(secciones[0]))
 
 main()
